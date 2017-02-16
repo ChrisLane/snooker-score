a931cee5bd285d4bde214f2b14db257294bdd7a1
Chris Lane <christopher_lane@live.co.uk>
2017-02-16 01:35:16 +0000

    Add share button

--- app/src/main/java/me/chrislane/snookerscore/Results.java
+++ app/src/main/java/me/chrislane/snookerscore/Results.java
@@ -3,15 +3,16 @@ package me.chrislane.snookerscore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class Results extends AppCompatActivity {
-    private String playerOneName;
-    private String playerTwoName;
-    private int playerOneScore;
-    private int playerTwoScore;
+    private String winner;
+    private String loser;
+    private int winningScore;
+    private int losingScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,8 @@ public class Results extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         Intent pastIntent = getIntent();
-        playerOneName = pastIntent.getStringExtra("playerOneName");
-        playerTwoName = pastIntent.getStringExtra("playerTwoName");
+        String playerOneName = pastIntent.getStringExtra("playerOneName");
+        String playerTwoName = pastIntent.getStringExtra("playerTwoName");
 
         TextView playerOneNameView = (TextView) findViewById(R.id.player_one);
         TextView playerTwoNameView = (TextView) findViewById(R.id.player_two);
@@ -28,8 +29,8 @@ public class Results extends AppCompatActivity {
         playerOneNameView.setText(playerOneName);
         playerTwoNameView.setText(playerTwoName);
 
-        playerOneScore = pastIntent.getIntExtra("playerOneScore", 0);
-        playerTwoScore = pastIntent.getIntExtra("playerTwoScore", 0);
+        int playerOneScore = pastIntent.getIntExtra("playerOneScore", 0);
+        int playerTwoScore = pastIntent.getIntExtra("playerTwoScore", 0);
 
         TextView playerOneScoreView = (TextView) findViewById(R.id.player_one_score);
         TextView playerTwoScoreView = (TextView) findViewById(R.id.player_two_score);
@@ -40,10 +41,26 @@ public class Results extends AppCompatActivity {
         TextView winningPlayerView = (TextView) findViewById(R.id.winning_player);
 
         if (playerOneScore > playerTwoScore) {
-            winningPlayerView.setText(playerOneName);
+            winner = playerOneName;
+            winningScore = playerOneScore;
+            loser = playerTwoName;
+            losingScore = playerTwoScore;
         } else {
-            winningPlayerView.setText(playerTwoName);
+            winner = playerTwoName;
+            winningScore = playerOneScore;
+            loser = playerOneName;
+            losingScore = playerTwoScore;
         }
 
+        winningPlayerView.setText(winner);
+    }
+
+    public void onClickShare(View view) {
+        String shareText = "%s won %d - %d against %s in a snooker frame";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Snooker Frame");
+        intent.putExtra(Intent.EXTRA_TEXT, String.format(Locale.ENGLISH, shareText, winner, winningScore, losingScore, loser));
+        startActivity(Intent.createChooser(intent, "Share via"));
     }
 }
